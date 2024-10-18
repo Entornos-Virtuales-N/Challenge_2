67565ce3a8ae47204ba186d960d8cfa8cd09ea43
KarlaTo97 <karlahernandezt04@gmail.com>
2024-10-18 01:12:45 -0600

    Challenge2

---
+++ Assets/Challenge 2/Scripts/DetectCollisionsX.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollisionsX : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Verifica si el objeto con el que colisiona es el perro
+        if (other.CompareTag("Dog"))
+        {
+            // Destruye la pelota
+            Destroy(gameObject);
+        }
+    }
+}
